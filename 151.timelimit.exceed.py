e276057677a90dba2a78e43272a79f946b4296d4
Jianer Shi <hipaulshi@gmail.com>
2020-06-01 09:02:28 -0500

    151 time limit exceed remove unnecessary functions

--- 151.timelimit.exceed.py
+++ 151.timelimit.exceed.py
@@ -17,23 +17,10 @@ class Solution:
     def max_profit_in_partition(self, prices, start_index, ending_index):
         if not (0 <= start_index < len(prices) and 0 <= ending_index <= len(prices)):
             return 0
-            
+
         smallest = sys.maxsize
         max_profit = -sys.maxsize
         for i in range(start_index, ending_index + 1):
             smallest = min(smallest, prices[i])
             max_profit = max(prices[i] - smallest, max_profit)
         return max_profit if max_profit > 0 else 0
-        
-    def build_smallest_number_from_left(self, prices):
-        if not prices:
-            return prices
-            
-        smallest_number_from_left = [prices[0]]
-        for price in prices:
-            last_price = smallest_number_from_left[-1]
-            if price < last_price:
-                smallest_number_from_left.append(price)
-            else:
-                smallest_number_from_left.append(last_price)
-        return smallest_number_from_left
