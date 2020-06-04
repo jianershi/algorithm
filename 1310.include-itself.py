25a6e65aadb235d800c09ce201445e70f2f03bea
Jianer Shi <hipaulshi@gmail.com>
2020-06-04 10:52:33 -0500

    1310 update comment. added 2nd version of inlcuding itself for prefix

---
+++ 1310.include-itself.py
@@ -0,0 +1,30 @@
+"""
+1310. Product of Array Except Self
+
+"""
+class Solution:
+    """
+    @param nums: an array of integers
+    @return: the product of all the elements of nums except nums[i].
+    """
+    def productExceptSelf(self, nums):
+        # write your code here
+        prefix_sum = self.build_prefix_product(nums)
+        suffix_sum = self.build_prefix_product(nums[::-1])[::-1]
+
+        result = [0] * len(nums)
+        for i in range(len(result)):
+            result[i] = prefix_sum[i - 1] * suffix_sum[i + 1]
+
+        return result
+
+    """
+    does include itself. so prefix_product[i] include everything form 0-i
+    """
+    def build_prefix_product(self, nums):
+        prefix_product = [None] * (len(nums))
+        prefix_product[0] = nums[0]
+        for i in range(1, len(nums)):
+            prefix_product[i] = prefix_product[i - 1] * nums[i]
+
+        return prefix_product
--- 1310.py
+++ 1310.py
@@ -1,3 +1,7 @@
+"""
+1310. Product of Array Except Self
+
+"""
 class Solution:
     """
     @param nums: an array of integers
@@ -11,7 +15,29 @@ class Solution:
         result = [0] * len(nums)
         for i in range(len(result)):
             result[i] = prefix_sum[i] * suffix_sum[i + 1]
+            """
+            index 0123
+                    ^
+            str   3841
+                    ^
+            prefix_sum = [1, 3, 24, 96, 1]
+            prefix_sum[2] = s[0] * s[1] = 3 * 8
 
+
+            index     3210
+                        ^
+            str[::-1] 1483
+                        ^
+            prefix_sum of flipped string = [1, 1, 4, 32, 96]
+            s[2] = 8
+            prefix_sum of flipped string [2] = 4
+
+            postfix_sum = prefix_sum of flipped string[::-1] = [96, 32, 4, 1, 1]
+            s[2] = 8
+            postfix_sum[2 + 1] = 4
+            that's why there is + 1
+
+            """
         return result
 
     def build_prefix_product(self, nums):
@@ -21,4 +47,3 @@ class Solution:
             prefix_product[i] = prefix_product[i - 1] * nums[i - 1]
 
         return prefix_product
-        
