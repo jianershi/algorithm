4f8b5885e3496cd78510286f323c62de2f2f8737
Jianer Shi <hipaulshi@gmail.com>
2020-06-05 08:47:51 -0500

    clean up 463 quick sort

--- 463.quick_sort.py
+++ 463.quick_sort.py
@@ -1,3 +1,8 @@
+"""
+463. Sort Integers
+quick sort
+
+"""
 class Solution:
     """
     @param A: an integer array
@@ -9,8 +14,6 @@ class Solution:
             return
 
         self.quick_sort(A, 0, len(A) - 1)
-        # temp = [0] * len(A)
-        # self.merge_sort(A, 0, len(A) - 1, temp)
 
     def quick_sort(self, nums, start, end):
         if start >= end:
@@ -29,35 +32,3 @@ class Solution:
 
         self.quick_sort(nums, start, right)
         self.quick_sort(nums, left, end)
-
-    # def merge_sort(self, nums, start, end, temp):
-    #     if start >= end:
-    #         return
-
-    #     self.merge_sort(nums, start, (start + end) // 2, temp)
-    #     self.merge_sort(nums, (start + end) // 2 + 1, end, temp)
-    #     self.merge_sort_merge(nums, start, end, temp)
-    # def merge_sort_merge(self, nums, start, end, temp):
-    #     mid = (start + end) // 2
-    #     index = start
-    #     left = start
-    #     right = mid + 1
-    #     while left <= mid and right <= end:
-    #         if nums[left] < nums[right]:
-    #             temp[index] = nums[left]
-    #             left += 1
-    #         else:
-    #             temp[index] = nums[right]
-    #             right += 1
-    #         index += 1
-
-    #     while left <= mid:
-    #         temp[index] = nums[left]
-    #         left += 1
-    #         index += 1
-    #     while right <= end:
-    #         temp[index] = nums[right]
-    #         right += 1
-    #         index += 1
-    #     for index in range(start, end + 1):
-    #         nums[index] = temp[index]
