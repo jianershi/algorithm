89eb68cdde7a172e19eda2229a0da1e693ef6381
Jianer Shi <hipaulshi@gmail.com>
2020-06-08 09:30:38 -0500

    partitioning array

---
+++ 31.py
@@ -0,0 +1,28 @@
+"""
+31. Partition Array
+
+"""
+class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+    def partitionArray(self, nums, k):
+        # write your code here
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            while left <= right and nums[left] < k:
+                left += 1
+            while left <= right and nums[right] >= k:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        return left
+
+s = Solution()
+nums = []
+k = 9
+print(s.partitionArray(nums,k))
