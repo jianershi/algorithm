5a58beff488705f44f8fc53ebc7ed2d8c4724cfb
Jianer Shi <hipaulshi@gmail.com>
2020-06-08 19:37:57 -0500

    384. Longest Substring Without Repeating Characters

---
+++ 384.py
@@ -0,0 +1,22 @@
+"""
+384. Longest Substring Without Repeating Characters
+"""
+class Solution:
+    """
+    @param s: a string
+    @return: an integer
+    """
+    def lengthOfLongestSubstring(self, s):
+        # write your code here
+        appeared = [False] * 256
+        n = len(s)
+        right = 0
+        max_length = 0
+        for left in range(n):
+            while right < n and not appeared[ord(s[right])]:
+                appeared[ord(s[right])] = True
+                max_length = max(max_length, right - left + 1)
+                right += 1
+
+            appeared[ord(s[left])] = False
+        return max_length
