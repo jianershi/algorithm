7e35b8862e80e86ad4d187eb976b37f6a28f71c0
Jianer Shi <hipaulshi@gmail.com>
2020-06-15 15:59:23 -0500

    363. Trapping Rain Water

--- 363.py
+++ 363.py
@@ -2,6 +2,9 @@
 363. Trapping Rain Water
 https://www.lintcode.com/problem/trapping-rain-water/description?_from=ladder&&fromId=106
 monotonous stack (decreasing)
+
+don't have to flush the stack. because if it is a decreasing stack, anything that is still remaining
+on the stack will be decreasing as well, which means, no water can hold to the right side of it.
 """
 import sys
 class Solution:
@@ -18,12 +21,13 @@ class Solution:
             curr = heights[i]
             while stack and curr >= heights[stack[-1]]:
                 top = stack.pop()
-                left = heights[stack[-1]] if stack else heights[top]
-                left_index = stack[-1] if stack else -1
+                left = heights[stack[-1]] if stack else heights[top] #cannot hold water for the 1st item
+                left_index = stack[-1] if stack else -1 #easier to calculate length
+                length = (i - left_index - 1)
                 if left < curr:
-                    water_capacity += (left - heights[top]) * (i - left_index - 1)
+                    water_capacity += (left - heights[top]) * length
                 else:
-                    water_capacity += (curr - heights[top]) * (i - left_index - 1)
+                    water_capacity += (curr - heights[top]) * length
             stack.append(i)
 
         return (water_capacity)
