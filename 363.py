785df992458660f7084dccd0b2a190a58cc8449c
Jianer Shi <hipaulshi@gmail.com>
2020-06-15 15:42:57 -0500

    363. Trapping Rain Water

---
+++ 363.py
@@ -0,0 +1,28 @@
+"""
+363. Trapping Rain Water
+https://www.lintcode.com/problem/trapping-rain-water/description?_from=ladder&&fromId=106
+"""
+import sys
+class Solution:
+    """
+    @param heights: a list of integers
+    @return: a integer
+    """
+    def trapRainWater(self, heights):
+        # write your code here
+        stack = []
+        water_capacity = 0
+        n = len(heights)
+        for i in range(n):
+            curr = heights[i]
+            while stack and curr >= heights[stack[-1]]:
+                top = stack.pop()
+                left = heights[stack[-1]] if stack else heights[top]
+                left_index = stack[-1] if stack else -1
+                if left < curr:
+                    water_capacity += (left - heights[top]) * (i - left_index - 1)
+                else:
+                    water_capacity += (curr - heights[top]) * (i - left_index - 1)
+            stack.append(i)
+
+        return (water_capacity)
