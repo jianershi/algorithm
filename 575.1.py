ce717b1d13b80280015a5d7db6b826f8ca29d823
Jianer Shi <hipaulshi@gmail.com>
2020-06-15 08:56:07 -0500

    575. Decode String

---
+++ 575.1.py
@@ -0,0 +1,27 @@
+"""
+575. Decode String
+https://www.lintcode.com/problem/decode-string/description
+"""
+class Solution:
+    """
+    @param s: an expression includes numbers, letters and brackets
+    @return: a string
+    """
+    def expressionExpand(self, s):
+        # write your code here
+        stack = []
+        number = 0
+        for c in s:
+            if c.isdigit():
+                number = 10 * number + (ord(c) - ord('0'))
+            elif c == '[':
+                stack.append(number)
+                number = 0
+            elif c == ']':
+                reversed_c = []
+                while stack and not isinstance(stack[-1], int):
+                    reversed_c.append(stack.pop())
+                stack.extend(reversed_c[::-1] * stack.pop())
+            else:
+                stack.append(c)
+        return "".join(stack)
