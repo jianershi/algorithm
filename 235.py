90c4615c4f2d02bcc37b199630ebac8cba6bcd9a
Jianer Shi <hipaulshi@gmail.com>
2020-06-17 16:41:39 -0500

    235. Prime Factorization

---
+++ 235.py
@@ -0,0 +1,24 @@
+"""
+235. Prime Factorization
+"""
+import math
+class Solution:
+    """
+    @param num: An integer
+    @return: an integer array
+    """
+    def primeFactorization(self, num):
+        result = []
+        up = math.sqrt(num)
+        k = 2
+
+        while k <= up and num > 1:
+            while num % k == 0:
+                result.append(k)
+                num //= k
+            k += 1
+
+        if num > 1:
+            result.append(num)
+
+        return result
