3729b7c8048a4bad045dbe4ad0d26e124a5d0fcf
Jianer Shi <hipaulshi@gmail.com>
2020-06-17 21:32:39 -0500

    97

---
+++ 97.py
@@ -0,0 +1,20 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """
+    def maxDepth(self, root):
+        # write your code here
+        if not root:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
