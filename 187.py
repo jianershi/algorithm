d263e87ba594f780da62d6cc5d9179a3e68302cf
Jianer Shi <hipaulshi@gmail.com>
2020-07-30 00:39:32 -0500

    187 gas station

--- 187.py
+++ 187.py
@@ -1,5 +1,6 @@
 """
-暴力dp写法，时间复杂度o(n^2)过不了
+187. Gas Station
+https://www.lintcode.com/problem/gas-station/description
 """
 class Solution:
     """
@@ -18,26 +19,13 @@ class Solution:
             dp[start_position] = 0
             for j in range(start_position + 1, start_position + len(gas) + 1):
                 dp[j % len(gas)] = dp[(j - 1) % len(gas)] + gas[(j - 1) % len(gas)] - cost[(j - 1) % len(gas)]
-                #print (dp)
                 if dp[j % len(gas)] < 0:
                     skip_rest = True
                     start_position = j
-                    #print(j)
                     break
             if skip_rest:
                 continue
-            possible = True
-            for i in range(len(dp)):
-                if dp[i] < 0:
-                    possible = False
-                    break
-            if possible and dp[start_position] >= 0:
+            if dp[start_position] >= 0:
                 return start_position
             start_position += 1
         return -1
-
-s = Solution()
-gas = [1,2,3,3]
-cost = [2,1,5,1]
-
-print(s.canCompleteCircuit(gas, cost))
