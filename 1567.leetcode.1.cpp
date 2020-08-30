259daf7907e4b190900cd365c4f48ef2e5ba6269
Jianer Shi <hipaulshi@gmail.com>
2020-08-30 13:05:56 -0700

    1567. Maximum Length of Subarray With Positive Product

---
+++ 1567.leetcode.1.cpp
@@ -0,0 +1,23 @@
+/**
+1567. Maximum Length of Subarray With Positive Product
+https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/
+**/
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int max_len = 0, first_neg = -1, zero_i = - 1, neg = 0, n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                neg = 0, first_neg = -1; zero_i = i;
+            } else if (nums[i] < 0) {
+                neg++;
+                if (first_neg == -1) first_neg = i;
+            } 
+            
+            if (neg % 2 == 0) max_len = max(max_len, i - zero_i);
+            else max_len = max(max_len, i - first_neg);
+        }
+        
+        return max_len;
+    }
+};
