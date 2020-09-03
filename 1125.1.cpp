0f86f447fd78ea6bbb4cba34e14f07bbe0bb8da1
Jianer Shi <hipaulshi@gmail.com>
2020-09-02 20:55:29 -0700

    1125. Jump Pillar rolling array

---
+++ 1125.1.cpp
@@ -0,0 +1,59 @@
+/**
+ * 1125. Jump Pillar
+ * https://www.lintcode.com/problem/jump-pillar/description
+ * DP
+ * dp[i][j] : whether can reach index i with j remaining chance
+ *
+ * not using super power
+ * dp[i][1] = dp[i - x][1] for 1 <= x <= k and dp[i] <= dp[i - k]
+ * 
+ * if 0 super power chance left, either use super power at this step or have already used it in the past.
+ * dp[i][0] = dp[i - x][1] | (dp[i - x][0] dp[i] <= dp[i - k]) for 1 <= x <= k
+ *
+ * answer:
+ * dp[n - 1][0] || dp [n - 1][1]
+ *
+ * intial conditon:
+ * dp[0][0] = true;
+ * dp[0][1] = true;
+ * regardless of using super power or not, one can always reach the first position.
+ */
+
+/**
+ * dp[i][j] : whether can reach prev ith position with j remaining chance
+ * dp[i][1] = dp[i - x][0] for 1 <= x <= k and dp[i - x] >= dp[i]
+ * dp[i][0] = dp[i - x][1] | dp[i - x][0] for 1 <= x <= k
+ * 
+ * initial dp[0][0] = true;
+ *
+ * 2d rolling array
+ * not very intuitive.
+ */ 
+class Solution {
+public:
+    /**
+     * @param h: the height of n pillar
+     * @param k: the limit
+     * @return: Xiao Yi can or can't reach the n-th pillar
+     */
+    bool jumpPillar(vector<int> &h, int k) {
+        // write your code here.
+        int n = h.size();
+        bool dp[k + 1][2];
+        memset(dp, false, sizeof dp);
+        dp[0][0] = true;
+        dp[0][1] = true;
+        for (int i = 1; i < n; ++i) {
+            dp[i % (k + 1)][0] = false;
+            dp[i % (k + 1)][1] = false;
+            for (int x = 1; x <= k && i - x >= 0; ++x) {
+                dp[i % (k + 1)][0] = dp[i % (k + 1)][0] || dp[(i - x) % (k + 1)][1];
+                if (h[i - x] >= h[i]) {
+                    dp[i % (k + 1)][0] = dp[i % (k + 1)][0] || dp[(i - x) % (k + 1)][0];
+                    dp[i % (k + 1)][1] = dp[i % (k + 1)][1] || dp[(i - x) % (k + 1)][1];
+                }
+            }
+        }
+        return dp[(n - 1) % (k + 1)][0] || dp[(n - 1) % (k + 1)][1];
+    }
+};
