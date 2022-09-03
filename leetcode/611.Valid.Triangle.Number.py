65f201b6f284bc09d5fea1a70707797d1da96b86
Jianer Shi <hipaulshi@gmail.com>
2022-09-03 10:32:12 +0900

    611. Valid Triangle Number

--- leetcode/611.Valid.Triangle.Number.py
+++ leetcode/611.Valid.Triangle.Number.py
@@ -1,5 +1,5 @@
 """
-382. Triangle Count
+611. Valid Triangle Number
 a+b > c
 any two side larger than the third one
 need to be smaller 2.
@@ -15,24 +15,17 @@ if 3 + 6 > 7:
 only need total number but don't need specific ways
 """
 class Solution:
-    """
-    @param S: A list of integers
-    @return: An integer
-    """
-    def triangleCount(self, nums):
-        # write your code here
+    def triangleNumber(self, nums: List[int]) -> int:
         nums = sorted(nums)
-        n = len(nums)
         count = 0
-        for i in range(n - 1, -1, -1):
+        for i in range(len(nums) - 1, -1, -1):
             left, right = 0, i - 1
-            while left < right: # < or < =? <
+            while left < right:
                 if left < right and nums[left] + nums[right] > nums[i]:
                     count += right - left
                     right -= 1
                 if left < right and nums[left] + nums[right] <= nums[i]:
                     left += 1
+        
         return count
-s = Solution()
-S = [2,2,2,2]
-print(s.triangleCount(S))
+        
