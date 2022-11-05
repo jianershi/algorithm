a895687d6b0832b921fb9a2aec480d1220890a51
Jianer Shi <hipaulshi@gmail.com>
2022-11-05 13:48:45 +0900

    521. Remove Duplicate Numbers in Array

--- lintcode/521.py
+++ lintcode/521.py
@@ -30,10 +30,10 @@ class Solution:
         这么写思路更加简单。
         首先i是正常遍历的，无论什么情况都到下一个，然后比较i和i-1的大小。假如不同的话，那就是正常。然后把慢的那个pointer指的地方用i指的值覆盖，这表示值要慢pointer经过的地方一定是unique的。要是相同了。慢指针就停了，快指针继续走到下一个不同的地方。然后继续往慢指针存。最后慢指针的index是多少就是有多少个unique number
         """
-        num_result = 1
+        write_pointer = 1
         for i in range(1, len(nums)):
             if nums[i - 1] != nums[i]:
-                nums[num_result] = nums[i]
-                num_result += 1
-        return num_result
+                nums[write_pointer] = nums[i]
+                write_pointer += 1
+        return write_pointer
                 
